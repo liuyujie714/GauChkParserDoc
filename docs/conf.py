8a93e458c73783582f3e5bf3a8fe17742dc2352d
liuyujie714 <1051690424@qq.com>
2025-07-24 11:40:46 +0800

    fix order

--- docs/conf.py
+++ docs/conf.py
@@ -1,34 +1,34 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'GauChkParser'
 copyright = '2025, Yujie Liu'
 author = 'Yujie Liu'
 release = __import__('GauChkParser').__version__
+version = release
 
 
 #import os
 #import sys
 #sys.path.insert(0, os.path.abspath('../../GauChkParser'))
 
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     'sphinx.ext.autodoc',
+#    'sphinx.ext.viewcode',  # 支持查看源码
     'sphinx.ext.napoleon',  # 支持 Doxygen 风格
-    'sphinx_markdown_tables', 'myst_parser' # 支持Markdown
+    'sphinx_markdown_tables', 'myst_parser', # 支持Markdown
 ]
 
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+
+# show python api order
+autodoc_member_order = 'bysource'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -41,3 +41,5 @@ source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
 }
+
+
